eb37b3c2c9d59bc1c0f09f911f5bde077a63f4da
Jaikumar Ganesh <jaikumar@google.com>
2009-07-17 16:55:42 -0700

    Rename cancelPin to cancelPairingUserInput for Bluetooth 2.1

--- src/com/android/bluetooth/opp/BluetoothPinDialog.java
+++ src/com/android/bluetooth/opp/BluetoothPinDialog.java
@@ -121,7 +121,7 @@ public class BluetoothPinDialog extends AlertActivity implements DialogInterface
     }
 
     private void onCancel() {
-        mLocalManager.getBluetoothManager().cancelPin(mAddress);
+        mLocalManager.getBluetoothManager().cancelPairingUserInput(mAddress);
     }
 
     public void onClick(DialogInterface dialog, int which) {
