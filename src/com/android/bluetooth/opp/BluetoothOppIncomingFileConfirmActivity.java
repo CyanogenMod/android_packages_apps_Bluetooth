d9d397854afd6511e2987ea7ae3ec8528183aefa
blunden <blunden2@gmail.com>
2012-09-19 00:57:46 +0200

    Upgrade the app to better follow holo guidelines
    - Fix the icon scaling in the transfer list
      The icon is centered and upscaled instead of just centered.
    
    - Remove dialog info icon according to UI team recommendation
    
    - Replace icons with holo versions and add xhdpi
    
    Change-Id: Id3b06e8e06244ec8859d65404d779b66c1354e3d

--- src/com/android/bluetooth/opp/BluetoothOppIncomingFileConfirmActivity.java
+++ src/com/android/bluetooth/opp/BluetoothOppIncomingFileConfirmActivity.java
@@ -106,7 +106,6 @@ public class BluetoothOppIncomingFileConfirmActivity extends AlertActivity imple
 
         // Set up the "dialog"
         final AlertController.AlertParams p = mAlertParams;
-        p.mIconId = android.R.drawable.ic_dialog_info;
         p.mTitle = getString(R.string.incoming_file_confirm_title);
         p.mView = createView();
         p.mPositiveButtonText = getString(R.string.incoming_file_confirm_ok);
