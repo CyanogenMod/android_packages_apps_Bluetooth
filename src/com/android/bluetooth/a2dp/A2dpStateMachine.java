be8695cc1e2d3e3c7760a4985ca437ac3bd11524
Gaurav Asati <gasati@codeaurora.org>
2013-09-12 17:10:53 +0530

    Properly update profile connection state.
    When DUT is toggled between various paired Headsets Connection
    state is not updated properly in some scenarios.This is causing
    Bluetooth icon to be in connected state even when no Headsets are
    connected.Also make sure connected,connecting,disconnecting devices
    count is never negative.
    
    Change-Id: Icb1a7215ff6d9491afbb5f7f203ca0a2c6b7f43a
    CRs-Fixed: 532783

--- src/com/android/bluetooth/a2dp/A2dpStateMachine.java
+++ src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -495,6 +495,9 @@ final class A2dpStateMachine extends StateMachine {
                         broadcastConnectionState(device, BluetoothProfile.STATE_DISCONNECTED,
                                        BluetoothProfile.STATE_CONNECTING);
                         break;
+                    } else {
+                        broadcastConnectionState(mCurrentDevice, BluetoothProfile.STATE_DISCONNECTING,
+                                       BluetoothProfile.STATE_CONNECTED);
                     }
 
                     synchronized (A2dpStateMachine.this) {
@@ -513,7 +516,7 @@ final class A2dpStateMachine extends StateMachine {
                                    BluetoothProfile.STATE_CONNECTED);
                     if (!disconnectA2dpNative(getByteAddress(device))) {
                         broadcastConnectionState(device, BluetoothProfile.STATE_CONNECTED,
-                                       BluetoothProfile.STATE_DISCONNECTED);
+                                       BluetoothProfile.STATE_DISCONNECTING);
                         break;
                     }
                     transitionTo(mPending);
