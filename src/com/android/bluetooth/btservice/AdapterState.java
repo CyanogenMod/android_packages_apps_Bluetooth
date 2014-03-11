0cadea57868725147cc6b7d1c709bfe1f1c226eb
Pradeep Panigrahi <pradeepp@codeaurora.org>
2014-03-11 11:45:03 +0530

    Bluetooth: Catch RuntimeException while setting system property
    Add change to catch RuntimeException while setting system property
    to prevent crash in bluetoothShare, if we are unable to set system
    property during BT on/off stress test.
    
    Change-Id: I3e89569639d4ecbd94fe805ab2350a259f4fec68
    CRs-fixed: 627455

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -25,6 +25,7 @@ import android.util.Log;
 import com.android.internal.util.State;
 import com.android.internal.util.StateMachine;
 import android.os.SystemProperties;
+import java.lang.RuntimeException;
 
 /**
  * This state machine handles Bluetooth Adapter State.
@@ -375,15 +376,19 @@ final class AdapterState extends StateMachine {
     }
 
     void stateChangeCallback(int status) {
-        if (status == AbstractionLayer.BT_STATE_OFF) {
-            SystemProperties.set("bluetooth.isEnabled","false");
-            sendMessage(DISABLED);
-        } else if (status == AbstractionLayer.BT_STATE_ON) {
-            // We should have got the property change for adapter and remote devices.
-            SystemProperties.set("bluetooth.isEnabled","true");
-            sendMessage(ENABLED_READY);
-        } else {
-            errorLog("Incorrect status in stateChangeCallback");
+        try {
+            if (status == AbstractionLayer.BT_STATE_OFF) {
+                SystemProperties.set("bluetooth.isEnabled","false");
+                sendMessage(DISABLED);
+            } else if (status == AbstractionLayer.BT_STATE_ON) {
+                // We should have got the property change for adapter and remote devices.
+                SystemProperties.set("bluetooth.isEnabled","true");
+                sendMessage(ENABLED_READY);
+            } else {
+                errorLog("Incorrect status in stateChangeCallback");
+            }
+        } catch (RuntimeException e) {
+                Log.e(TAG,"Error setting system prop " + e);
         }
     }
 
