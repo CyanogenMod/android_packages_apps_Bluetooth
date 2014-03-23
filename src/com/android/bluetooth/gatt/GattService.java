18693d9f8a412a02182f1de922bdfecfe77440ee
Sunny Kapdi <sunnyk@codeaurora.org>
2014-03-22 21:51:42 -0700

    BluetoothGatt: Use ServerIf during registration
    Use serverIf for active service declaration to fix
    issues with certain Apps that don't wait on
    onServiceAdded() before registering the next service
    
    Change-Id: I6e329ddaf53611d81f56be1d98508564283dd377
    CRs-Fixed: 632265

--- src/com/android/bluetooth/gatt/GattService.java
+++ src/com/android/bluetooth/gatt/GattService.java
@@ -107,19 +107,37 @@ public class GattService extends ProfileService {
      */
     private List<ServiceDeclaration> mServiceDeclarations = new ArrayList<ServiceDeclaration>();
 
-    private ServiceDeclaration addDeclaration() {
-        synchronized (mServiceDeclarations) {
-            mServiceDeclarations.add(new ServiceDeclaration());
+    /**
+     * Temporary App (active) service declaration queue
+     */
+    private HashMap<Integer, ServiceDeclaration> mActiveServiceDeclarations = new HashMap<Integer, ServiceDeclaration>();
+
+    private ServiceDeclaration addToActiveDeclaration(int serverIf) {
+        synchronized (mActiveServiceDeclarations) {
+            mActiveServiceDeclarations.put(serverIf, new ServiceDeclaration());
         }
-        return getActiveDeclaration();
+        return getActiveDeclaration(serverIf);
     }
 
-    private ServiceDeclaration getActiveDeclaration() {
-        synchronized (mServiceDeclarations) {
-            if (mServiceDeclarations.size() > 0)
-                return mServiceDeclarations.get(mServiceDeclarations.size() - 1);
+    private void removeFromActiveDeclaration(int serverIf) {
+        synchronized (mActiveServiceDeclarations) {
+            mActiveServiceDeclarations.remove(serverIf);
+        }
+    }
+
+    private ServiceDeclaration getActiveDeclaration(int serverIf) {
+        synchronized (mActiveServiceDeclarations) {
+            if (mActiveServiceDeclarations.size() > 0)
+                return mActiveServiceDeclarations.get(serverIf);
         }
         return null;
+    }
+
+
+    private void addToPendingDeclaration(ServiceDeclaration serviceDeclaration) {
+        synchronized (mServiceDeclarations) {
+            mServiceDeclarations.add(serviceDeclaration);
+        }
     }
 
     private ServiceDeclaration getPendingDeclaration() {
@@ -176,6 +194,7 @@ public class GattService extends ProfileService {
         mSearchQueue.clear();
         mHandleMap.clear();
         mServiceDeclarations.clear();
+        mActiveServiceDeclarations.clear();
         mReliableQueue.clear();
         if (mAdvertiseManager != null) mAdvertiseManager.cleanup();
         if (mScanManager != null) mScanManager.cleanup();
@@ -1869,8 +1888,9 @@ public class GattService extends ProfileService {
                                  int minHandles, UUID srvcUuid, boolean advertisePreferred) {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
 
-        if (DBG) Log.d(TAG, "beginServiceDeclaration() - uuid=" + srvcUuid);
-        ServiceDeclaration serviceDeclaration = addDeclaration();
+        if (DBG) Log.d(TAG, "beginServiceDeclaration() - uuid=" + srvcUuid +
+                                                       " serverIf=" + serverIf);
+        ServiceDeclaration serviceDeclaration = addToActiveDeclaration(serverIf);
         serviceDeclaration.addService(srvcUuid, srvcType, srvcInstanceId, minHandles,
             advertisePreferred);
     }
@@ -1880,7 +1900,7 @@ public class GattService extends ProfileService {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
 
         if (DBG) Log.d(TAG, "addIncludedService() - uuid=" + srvcUuid);
-        getActiveDeclaration().addIncludedService(srvcUuid, srvcType, srvcInstanceId);
+        getActiveDeclaration(serverIf).addIncludedService(srvcUuid, srvcType, srvcInstanceId);
     }
 
     void addCharacteristic(int serverIf, UUID charUuid, int properties,
@@ -1888,28 +1908,31 @@ public class GattService extends ProfileService {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
 
         if (DBG) Log.d(TAG, "addCharacteristic() - uuid=" + charUuid);
-        getActiveDeclaration().addCharacteristic(charUuid, properties, permissions);
+        getActiveDeclaration(serverIf).addCharacteristic(charUuid, properties, permissions);
     }
 
     void addDescriptor(int serverIf, UUID descUuid, int permissions) {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
 
         if (DBG) Log.d(TAG, "addDescriptor() - uuid=" + descUuid);
-        getActiveDeclaration().addDescriptor(descUuid, permissions);
+        getActiveDeclaration(serverIf).addDescriptor(descUuid, permissions);
     }
 
     void endServiceDeclaration(int serverIf) {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
 
-        if (DBG) Log.d(TAG, "endServiceDeclaration()");
+        if (DBG) Log.d(TAG, "endServiceDeclaration()" + " serverIf=" + serverIf);
 
-        if (getActiveDeclaration() == getPendingDeclaration()) {
+        addToPendingDeclaration(getActiveDeclaration(serverIf));
+
+        if (getActiveDeclaration(serverIf) == getPendingDeclaration()) {
             try {
                 continueServiceDeclaration(serverIf, (byte)0, 0);
             } catch (RemoteException e) {
                 Log.e(TAG,""+e);
             }
         }
+        removeFromActiveDeclaration(serverIf);
     }
 
     void removeService(int serverIf, int srvcType,
