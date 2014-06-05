8812b408f52e55394195c4827eb587a610b054c9
Matthew Xie <mattx@google.com>
2014-06-05 07:23:34 -0700

    Resolved Bluetooth enabling bug in JNI
    Modified the Adapter service JNI to treat BT_STATUS_DONE
    as success event  and pass the success status to java layer.
    
    Change-Id: Iebb95820d263027376c2a1a0372ae7800e77872e

--- jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -678,7 +678,7 @@ static jboolean enableNative(JNIEnv* env, jobject obj) {
     if (!sBluetoothInterface) return result;
 
     int ret = sBluetoothInterface->enable();
-    result = (ret == BT_STATUS_SUCCESS) ? JNI_TRUE : JNI_FALSE;
+    result = (ret == BT_STATUS_SUCCESS || ret == BT_STATUS_DONE) ? JNI_TRUE : JNI_FALSE;
     return result;
 }
 
