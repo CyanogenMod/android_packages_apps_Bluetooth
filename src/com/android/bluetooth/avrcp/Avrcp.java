8e0d927632cce4ad53df85fb7627b915c5fbd64b
RoboErik <epastern@google.com>
2014-06-24 14:05:00 -0700

    Disable debug messages that were left on
    Forgot to disable with my last change.
    
    Change-Id: I502e77071745bd5f9d98aa9ce963b927effc2d46

--- src/com/android/bluetooth/avrcp/Avrcp.java
+++ src/com/android/bluetooth/avrcp/Avrcp.java
@@ -61,7 +61,7 @@ import java.util.Set;
  * support metadata, play status and event notification
  */
 public final class Avrcp {
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private static final String TAG = "Avrcp";
 
     private Context mContext;
