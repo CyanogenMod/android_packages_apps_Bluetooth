91901f047dca8abbfbacfd0bd7380f8e55388fe7
Scott Mertz <smertz@cyngn.com>
2014-07-24 15:20:27 -0700

    Bluetooth: Prevent race condition when authorizing
    Change-Id: Ic875fd619afea5fd36ce9b2f71a91d2d68dbb578

--- src/com/android/bluetooth/map/BluetoothMapService.java
+++ src/com/android/bluetooth/map/BluetoothMapService.java
@@ -821,6 +821,7 @@ public class BluetoothMapService extends ProfileService {
                         }
                      } else {
                         mConnectionManager.setWaitingForConfirmation(mMasId);
+                        isWaitingAuthorization = true;
                         Intent intent = new
                             Intent(BluetoothDevice.ACTION_CONNECTION_ACCESS_REQUEST);
                         intent.setClassName(ACCESS_AUTHORITY_PACKAGE, ACCESS_AUTHORITY_CLASS);
@@ -828,7 +829,6 @@ public class BluetoothMapService extends ProfileService {
                                         BluetoothDevice.REQUEST_TYPE_MESSAGE_ACCESS);
                         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, mRemoteDevice);
                         sendBroadcast(intent, BLUETOOTH_ADMIN_PERM);
-                        isWaitingAuthorization = true;
 
                         if (DEBUG) Log.d(TAG, "waiting for authorization for connection from: "
                                 + sRemoteDeviceName);
