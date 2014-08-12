ea0ae162549ea6f1750fc91dc36bffbba8a8852e
Juffin Alex Varghese <jalex@codeaurora.org>
2014-08-12 12:23:19 +0530

    Bluetooth: Handle IllegalArgumentException for broadcast receiver
    This change will handle IllegalArgumentExceptions during register or
    unregister broadcast receiver. Otherwise application will crash during
    register or unregister broadcast receiver because of IllegalArgument
    Exception.
    
    CRs-Fixed: 712224
    Change-Id: I0ab509f4d864d5f41eac7766e9cbff1e46dbf9ce

--- src/com/android/bluetooth/btservice/AdapterProperties.java
+++ src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -97,8 +97,12 @@ class AdapterProperties {
             mProfileConnectionState.clear();
         }
         mRemoteDevices = remoteDevices;
-        IntentFilter filter = new IntentFilter(Intent.ACTION_USER_SWITCHED);
-        mContext.registerReceiver(mBluetoothReceiver, filter, null, null);
+        try {
+            IntentFilter filter = new IntentFilter(Intent.ACTION_USER_SWITCHED);
+            mContext.registerReceiver(mBluetoothReceiver, filter, null, null);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Exception: registerReceiver");
+        }
     }
 
     public void cleanup() {
@@ -110,7 +114,11 @@ class AdapterProperties {
         mService = null;
         if (!mBondedDevices.isEmpty())
             mBondedDevices.clear();
-        mContext.unregisterReceiver(mBluetoothReceiver);
+        try {
+            mContext.unregisterReceiver(mBluetoothReceiver);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Exception: unregisterReceiver");
+        }
         mContext = null;
     }
 
