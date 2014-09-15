ce70a0292cec585f235391366221846207b0e549
Natiq Ahmed <mnatiq@codeaurora.org>
2014-09-15 17:04:36 +0530

    Revert "Make directory structure + Java packages consistent."
    This reverts commit 31db28fcd2e2e1840e03b4bff7f121ed6dd47344.
    
    Change-Id: I1d7dc7aff472ea7de885100d21415b60e0b8f989

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-        $(call all-java-files-under, lib/mapapi)
+        $(call all-java-files-under, lib)
 
 LOCAL_MODULE := bluetooth.mapsapi
 
