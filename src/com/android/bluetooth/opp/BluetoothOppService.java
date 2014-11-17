0cb71bf79ed4b70bc6b254950a1683d72b7e1ed5
Matthew Xie <mattx@google.com>
2014-11-17 09:50:54 -0800

    Check previous user confirmation before auto-confirm put request
    Also correct a confirm status change check in updateShare.
    Bug: 17770561, 18343032
    
    Change-Id: I8e7d10e73604c0bf1c88801a1caef7d579fbd1eb

--- src/com/android/bluetooth/opp/BluetoothOppService.java
+++ src/com/android/bluetooth/opp/BluetoothOppService.java
@@ -821,7 +821,7 @@ public class BluetoothOppService extends Service {
         info.mDestination = stringFromCursor(info.mDestination, cursor, BluetoothShare.DESTINATION);
         int newVisibility = cursor.getInt(cursor.getColumnIndexOrThrow(BluetoothShare.VISIBILITY));
 
-        boolean confirmed = false;
+        boolean confirmUpdated = false;
         int newConfirm = cursor.getInt(cursor
                 .getColumnIndexOrThrow(BluetoothShare.USER_CONFIRMATION));
 
@@ -834,10 +834,8 @@ public class BluetoothOppService extends Service {
         info.mVisibility = newVisibility;
 
         if (info.mConfirm == BluetoothShare.USER_CONFIRMATION_PENDING
-            && (newConfirm == BluetoothShare.USER_CONFIRMATION_CONFIRMED ||
-                newConfirm == BluetoothShare.USER_CONFIRMATION_AUTO_CONFIRMED ||
-                newConfirm == BluetoothShare.USER_CONFIRMATION_HANDOVER_CONFIRMED)) {
-            confirmed = true;
+                && newConfirm != BluetoothShare.USER_CONFIRMATION_PENDING) {
+            confirmUpdated = true;
         }
         info.mConfirm = newConfirm;
         int newStatus = cursor.getInt(statusColumn);
@@ -854,14 +852,14 @@ public class BluetoothOppService extends Service {
         info.mTimestamp = cursor.getLong(cursor.getColumnIndexOrThrow(BluetoothShare.TIMESTAMP));
         info.mMediaScanned = (cursor.getInt(cursor.getColumnIndexOrThrow(Constants.MEDIA_SCANNED)) != Constants.MEDIA_SCANNED_NOT_SCANNED);
 
-        if (confirmed) {
-            if (V) Log.v(TAG, "Service handle info " + info.mId + " confirmed");
-            /* Inbounds transfer get user confirmation, so we start it */
+        if (confirmUpdated) {
+            if (V) Log.v(TAG, "Service handle info " + info.mId + " confirmation updated");
+            /* Inbounds transfer user confirmation status changed, update the session server */
             int i = findBatchWithTimeStamp(info.mTimestamp);
             if (i != -1) {
                 BluetoothOppBatch batch = mBatchs.get(i);
                 if (mServerTransfer != null && batch.mId == mServerTransfer.getBatchId()) {
-                    mServerTransfer.setConfirmed();
+                    mServerTransfer.confirmStatusChanged();
                 } //TODO need to think about else
             }
         }
@@ -1006,7 +1004,7 @@ public class BluetoothOppService extends Service {
                         BluetoothOppShareInfo mPendingShare = nextBatch.getPendingShare();
                         if ((mPendingShare != null) && (mPendingShare.mConfirm ==
                                 BluetoothShare.USER_CONFIRMATION_CONFIRMED)) {
-                            mServerTransfer.setConfirmed();
+                            mServerTransfer.confirmStatusChanged();
                         }
                         return;
                     }
