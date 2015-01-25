d72320d82bd494863704458e8387269481b72e12
Scott Warner <Tortel1210@gmail.com>
2015-01-25 14:04:53 -0500

    Handle change from boolean to int preference
    Commit 02bf7ae9826a355db8614d1aad4f04bb5d6509ab changed the type of the
    phonebook access preference from boolean to int, which throws a
    ClassCastException and disconnects from the device.
    This changes the preference type from a boolean to an integer if this
    would happen
    
    Change-Id: I37bc1f4eec52cd05a04d370bfcecb473c0eff374

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1768,7 +1768,17 @@ public class AdapterService extends Service {
         if (!pref.contains(device.getAddress())) {
             return BluetoothDevice.ACCESS_UNKNOWN;
         }
-        return pref.getInt(device.getAddress(), BluetoothDevice.ACCESS_UNKNOWN);
+        try {
+            return pref.getInt(device.getAddress(), BluetoothDevice.ACCESS_UNKNOWN);
+        } catch (ClassCastException e) {
+            // Handle and change old boolean preferences to the int-based values
+            boolean currentPref = pref.getBoolean(device.getAddress(), false);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putInt(device.getAddress(), currentPref ?
+                BluetoothDevice.ACCESS_ALLOWED : BluetoothDevice.ACCESS_REJECTED);
+            editor.commit();
+            return pref.getInt(device.getAddress(), BluetoothDevice.ACCESS_UNKNOWN);
+        }
     }
 
     boolean setPhonebookAccessPermission(BluetoothDevice device, int value) {
