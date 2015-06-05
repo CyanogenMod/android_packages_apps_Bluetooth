b520ad2140b0e09c18dda4f004ee556122f7d206
Pradeep Panigrahi <pradeepp@codeaurora.org>
2015-06-05 12:03:45 +0530

    Handle file send retry scenario inside thread to avoid ANR.
    Use Case:
    1. Turn on BT and pair with the other device via BT.
    2. Launch Contacts ->press menu -> "Import/export" ->
       "Share visible contacts" -> "Bluetooth" -> "select
        paired device".
    3. Turn off BT and let the transfer fail
    4. Drag down the status bar and tap the bluetooth share notification
    5. Wait for a while, tap "try again"
    
    Failure:
    ANR occurs and "Unfortunately,Bluetooth share has stopped" error comes.
    
    Root Cause:
    BluetoothOppSendFileInfo::generateFileInfo() takes long time to process
    when there is huge contact list thereby causing ANR
    
    Fix:
    When tap the "try again" button on the send failed dialog, the
    method "BluetoothOppSendFileInfo::generateFileInfo()" will be
    call, if there are about 1500 contacts, then this method would
    be very time consuming, therby causing ANR. Changed this method
    into the child thread execution and through a Handler send a
    message to the main thread after this method return, to retry failed
    transfer.
    
    Change-Id: Idde7e874e4e25caba59133e0981e812b06ba2ed1

--- src/com/android/bluetooth/opp/BluetoothOppTransferActivity.java
+++ src/com/android/bluetooth/opp/BluetoothOppTransferActivity.java
@@ -41,6 +41,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -68,6 +69,9 @@ public class BluetoothOppTransferActivity extends AlertActivity implements
     private static final String TAG = "BluetoothOppTransferActivity";
     private static final boolean D = Constants.DEBUG;
     private static final boolean V = Constants.VERBOSE;
+
+    private UpdateHandler mHandler;
+    private final int GENERATE_SEND_FILE_INFO_OK = 1;
 
     private Uri mUri;
 
@@ -163,6 +167,8 @@ public class BluetoothOppTransferActivity extends AlertActivity implements
 
         // Set up the "dialog"
         setUpDialog();
+
+        mHandler = new UpdateHandler();
     }
 
     @Override
@@ -346,6 +352,34 @@ public class BluetoothOppTransferActivity extends AlertActivity implements
         }
     }
 
+    private class UpdateHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == GENERATE_SEND_FILE_INFO_OK) {
+                if (D) Log.d(TAG, "GENERATE_SEND_FILE_INFO_OK");
+                // make current transfer "hidden"
+                BluetoothOppUtility.updateVisibilityToHidden(BluetoothOppTransferActivity.this,
+                        mUri);
+
+                // clear correspondent notification item
+                ((NotificationManager)getSystemService(NOTIFICATION_SERVICE))
+                        .cancel(mTransInfo.mID);
+
+                // retry the failed transfer
+                BluetoothOppUtility.retryTransfer(BluetoothOppTransferActivity.this, mTransInfo);
+
+                BluetoothDevice remoteDevice = (BluetoothDevice)msg.obj;
+                // Display toast message
+                Toast.makeText(BluetoothOppTransferActivity.this,
+                        BluetoothOppTransferActivity.this.getString(R.string.bt_toast_4,
+                            BluetoothOppManager.getInstance(BluetoothOppTransferActivity.this)
+                                .getDeviceName(remoteDevice)), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+       }
+    };
+
     public void onClick(DialogInterface dialog, int which) {
         switch (which) {
             case DialogInterface.BUTTON_POSITIVE:
@@ -363,25 +397,27 @@ public class BluetoothOppTransferActivity extends AlertActivity implements
                 } else if (mWhichDialog == DIALOG_SEND_COMPLETE_FAIL) {
                     // "try again"
 
-                    // make current transfer "hidden"
-                    BluetoothOppUtility.updateVisibilityToHidden(this, mUri);
+                    new Thread("putSendFileInfo") {
+                        Uri uri = BluetoothOppUtility.originalUri(Uri.parse(mTransInfo.mFileUri));
+                        public void run() {
+                            BluetoothOppSendFileInfo sendFileInfo =
+                                BluetoothOppSendFileInfo.generateFileInfo(BluetoothOppTransferActivity.this,
+                                uri, mTransInfo.mFileType);
+                            uri = BluetoothOppUtility.generateUri(uri, sendFileInfo);
+                            BluetoothOppUtility.putSendFileInfo(uri, sendFileInfo);
+                            mTransInfo.mFileUri = uri.toString();
 
-                    // clear correspondent notification item
-                    ((NotificationManager)getSystemService(NOTIFICATION_SERVICE))
-                            .cancel(mTransInfo.mID);
-
-                    // retry the failed transfer
-                    BluetoothOppUtility.retryTransfer(this, mTransInfo);
-
-                    BluetoothDevice remoteDevice = mAdapter.getRemoteDevice(mTransInfo.mDestAddr);
-
-                    // Display toast message
-                    Toast.makeText(
-                            this,
-                            this.getString(R.string.bt_toast_4, BluetoothOppManager.getInstance(
-                                    this).getDeviceName(remoteDevice)), Toast.LENGTH_SHORT)
-                            .show();
-
+                            BluetoothDevice remoteDevice =
+                                mAdapter.getRemoteDevice(mTransInfo.mDestAddr);
+                            Message message = mHandler.obtainMessage();
+                            message.obj = remoteDevice;
+                            message.what = GENERATE_SEND_FILE_INFO_OK;
+                            message.sendToTarget();
+                        }
+                    }.start();
+                    Toast.makeText(this, getString(R.string.upload_fail_waiting),
+                            Toast.LENGTH_LONG).show();
+                    return;
                 } else if (mWhichDialog == DIALOG_SEND_COMPLETE_SUCCESS) {
                     BluetoothOppUtility.updateVisibilityToHidden(this, mUri);
                     ((NotificationManager)getSystemService(NOTIFICATION_SERVICE))
