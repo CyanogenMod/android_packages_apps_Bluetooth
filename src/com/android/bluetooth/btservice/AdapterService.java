5b6d0339a47a730580efa6c4236eef365dbb5343
Ajay Panicker <apanicke@google.com>
2015-09-02 16:20:15 -0700

    Prevent NullPointerException on AdapterService.mRemoteDevices
    Makes sure that mRemoteDevices is always initialized and not null
    even if AdapterService functions are called while Ble is turned
    off. Before this if some AdapterService functions were ever used
    before Ble was turned on, mRemoteDevices wouldn't have been
    initialized and a NPE would occur.
    
    Bug: 21893759
    Change-Id: Ia0f41c3d4fbfc6322d49431d1f5021857f018ff0

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -196,6 +196,12 @@ public class AdapterService extends Service {
                 debugLog("AdapterService() - REFCOUNT: CREATED. INSTANCE_COUNT" + sRefCount);
             }
         }
+
+        // This is initialized at the beginning in order to prevent
+        // NullPointerException from happening if AdapterService
+        // functions are called before BLE is turned on due to
+        // |mRemoteDevices| being null.
+        mRemoteDevices = new RemoteDevices(this);
     }
 
     public void onProfileConnectionStateChanged(BluetoothDevice device, int profileId, int newState, int prevState) {
@@ -431,7 +437,17 @@ public class AdapterService extends Service {
         for (int i=0; i < supportedProfileServices.length;i++) {
             mProfileServicesState.put(supportedProfileServices[i].getName(),BluetoothAdapter.STATE_OFF);
         }
-        mRemoteDevices = new RemoteDevices(this);
+
+        // Reset |mRemoteDevices| whenever BLE is turned off then on
+        // This is to replace the fact that |mRemoteDevices| was
+        // reinitialized in previous code.
+        //
+        // TODO(apanicke): The reason is unclear but
+        // I believe it is to clear the variable every time BLE was
+        // turned off then on. The same effect can be achieved by
+        // calling cleanup but this may not be necessary at all
+        // We should figure out why this is needed later
+        mRemoteDevices.cleanup();
         mAdapterProperties.init(mRemoteDevices);
 
         debugLog("BleOnProcessStart() - Make Bond State Machine");
