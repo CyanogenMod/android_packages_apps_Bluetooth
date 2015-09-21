2dd6813dc36bff3bbb6c81f61b5321b0269673fb
Hemant Gupta <hemantg@codeaurora.org>
2015-09-21 20:59:07 +0530

    MAP: Synchronize notifcationReg reply and MNS connect action.
    Use Case:
    1. Connect to carkit
    2. Disconnect connection with carkit.
    3. Reconnect to carkit
    
    Failure:
    MNS Failure during Reconnection.
    User will in some timing conditions not get notification
    of new messages and then turns OFF/ON the carkit.
    MAS is connected fine but for MNS car sends BAD REQUEST.
    
    Root Cause:
    This isssue is reported only with one particular carkit in MCE role,
    where in MNS Connect request issued before acknowleding
    the setNotificationReg Request.
    
    Fix:
    Synchronize handleRegistration method in handler thread to ensure
    setNotificationReg response from MSE is sent before requesting
    a MNS Connect to MCE.
    
    Change-Id: I30559c643af7733754fce551b97e31543a0cb160

--- src/com/android/bluetooth/map/BluetoothMnsObexClient.java
+++ src/com/android/bluetooth/map/BluetoothMnsObexClient.java
@@ -153,7 +153,7 @@ public class BluetoothMnsObexClient {
     /**
      * Shutdown the MNS.
      */
-    public void shutdown() {
+    public synchronized void shutdown() {
         /* should shutdown handler thread first to make sure
          * handleRegistration won't be called when disconnect
          */
@@ -178,7 +178,7 @@ public class BluetoothMnsObexClient {
      * @param masId
      * @param notificationStatus
      */
-    public void handleRegistration(int masId, int notificationStatus){
+    public synchronized void handleRegistration(int masId, int notificationStatus){
         if(D) Log.d(TAG, "handleRegistration( " + masId + ", " + notificationStatus + ")");
 
         if(notificationStatus == BluetoothMapAppParams.NOTIFICATION_STATUS_NO) {
