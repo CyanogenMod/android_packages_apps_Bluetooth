c1d31a0ad35991882bf4ca13470b3ba088c4520c
Satish Kodishala <skodisha@codeaurora.org>
2015-09-22 16:27:14 +0530

    HF: Do not remove connect defer messages in Connected state
    When we are in AudioOn state and  headset2 is connected,
    we disconnect SCO and move to Connected state. If we remove the
    connect defer message from the queue, we will not be able to
    connect to the headset2.
    
    CRs-fixed: 916103
    Change-Id: Ideb077108ab6b7e7a0caf0f881960c7c53bbd330

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -803,13 +803,7 @@ final class HeadsetStateMachine extends StateMachine {
     private class Connected extends State {
         @Override
         public void enter() {
-            // Remove pending connection attempts that were deferred during the pending
-            // state. This is to prevent auto connect attempts from disconnecting
-            // devices that previously successfully connected.
-            // TODO: This needs to check for multiple HFP connections, once supported...
-            removeDeferredMessages(CONNECT);
-
-            log("Enter Connected: " + getCurrentMessage().what +
+            Log.d(TAG, "Enter Connected: " + getCurrentMessage().what +
                            ", size: " + mConnectedDevicesList.size());
             // start phone state listener here so that the CIND response as part of SLC can be
             // responded to, correctly.
