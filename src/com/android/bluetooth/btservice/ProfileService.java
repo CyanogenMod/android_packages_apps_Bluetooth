3e3f7efe1abc723c6fd95ec0a24c76fbe4c3a772
Ajay Kumar <ajayku@codeaurora.org>
2016-01-21 20:23:40 +0530

    Bluetooth: Replying more on AdapterState than Intent
    While starting profile we sent the intent for every profile
    up on receiving this intent profile starts. If this intent is
    delayed and meanwhile adapterState is changed, we are starting
    profiles only based on state received by intent. Confirming
    the same via adapter should resolve issue.
    
    CRs-Fixed: 956218
    Change-Id: Iff7d078957540386b232c404c65ed8cda1510564

--- src/com/android/bluetooth/btservice/ProfileService.java
+++ src/com/android/bluetooth/btservice/ProfileService.java
@@ -140,8 +140,23 @@ public abstract class ProfileService extends Service {
                     Log.d(mName, "Received stop request...Stopping profile...");
                     doStop(intent);
                 } else if (state == BluetoothAdapter.STATE_ON) {
-                    Log.d(mName, "Received start request. Starting profile...");
-                    doStart(intent);
+                          if ((mAdapter.getLeState()== BluetoothAdapter.STATE_TURNING_ON &&
+                              !mName.equals("BtGatt.GattService")) ||
+                              (mAdapter.getLeState()== BluetoothAdapter.STATE_BLE_TURNING_ON &&
+                              mName.equals("BtGatt.GattService")) ) {
+
+                              Log.d(mName, "Received start request. Starting profile...");
+                              mAdapterService = AdapterService.getAdapterService();
+                              if (mAdapterService != null) {
+                                  mAdapterService.addProfile(this);
+                              } else {
+                                  Log.w(TAG, "onStart, null mAdapterService, this should never happen ");
+                              }
+
+                              doStart(intent);
+                          } else {
+                              Log.e(mName, ":intent received late, not starting profile");
+                          }
                 }
             }
         }
