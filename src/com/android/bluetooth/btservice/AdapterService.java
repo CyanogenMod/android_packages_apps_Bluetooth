11976279d5403fb65d9f0ea72b0e450dfd841958
Jakub Pawlowski <jpawlowski@google.com>
2016-04-15 05:30:01 -0700

    Fix work profile app not being able to create bond
    Bug: 28153078
    Change-Id: I49c020d7d016c667a6cb3366ef862a1eb9dfb4d5

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1071,7 +1071,7 @@ public class AdapterService extends Service {
         }
 
         public boolean createBond(BluetoothDevice device, int transport) {
-            if (!Utils.checkCaller()) {
+            if (!Utils.checkCallerAllowManagedProfiles(mService)) {
                 Log.w(TAG, "createBond() - Not allowed for non-active user");
                 return false;
             }
@@ -1082,7 +1082,7 @@ public class AdapterService extends Service {
         }
 
         public boolean createBondOutOfBand(BluetoothDevice device, int transport, OobData oobData) {
-            if (!Utils.checkCaller()) {
+            if (!Utils.checkCallerAllowManagedProfiles(mService)) {
                 Log.w(TAG, "createBondOutOfBand() - Not allowed for non-active user");
                 return false;
             }
