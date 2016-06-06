983e5e14f9d4ef8e42876f67227777f1b6510ba0
Hemant Gupta <hemantg@codeaurora.org>
2016-06-06 23:03:19 +0530

    MAP: Remove redundant call to sendShutdownMessage to prevent crash
    Usecase:
    Perform BT off/on repeatedly.
    
    Issue:
    Bluetooth crashes.
    
    Fix:
    Remove redundant call to sendShutdownMessage() from stop API to prevent
    MapServiceMessageHandler becoming null and being dereferenced.
    
    Bug: 29149190
    Change-Id: I739664975043c4a1895fd2113869c87cdb704338
    (cherry picked from commit 5847ca3c7068500b1f2a4cd4cf5ff373347f8fbf)

--- src/com/android/bluetooth/map/BluetoothMapService.java
+++ src/com/android/bluetooth/map/BluetoothMapService.java
@@ -828,7 +828,6 @@ public class BluetoothMapService extends ProfileService {
         }
         mStartError = true;
         setState(BluetoothMap.STATE_DISCONNECTED, BluetoothMap.RESULT_CANCELED);
-        sendShutdownMessage();
         return true;
     }
 
