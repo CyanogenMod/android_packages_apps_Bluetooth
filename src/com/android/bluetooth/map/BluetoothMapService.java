f93eda784aacc40a1d2df8c4390c69378cdeab74
Jessica Wagantall <jwagantall@cyngn.com>
2016-06-14 10:48:31 -0700

    Revert ""DO NOT MERGE" Add write SMS protection"
    CYNGNOS-3020
    This reverts commit a19e04f9b78b13088f8c2d5d86c6b416afa0c81b.
    
    Change-Id: I1debdc816ddefcfb1a7e6e196cc0dba7c4b426a5

--- src/com/android/bluetooth/map/BluetoothMapService.java
+++ src/com/android/bluetooth/map/BluetoothMapService.java
@@ -38,7 +38,6 @@ import android.bluetooth.BluetoothMap;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
-import android.Manifest;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
@@ -426,10 +425,6 @@ public class BluetoothMapService extends ProfileService {
         filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         try {
             registerReceiver(mMapReceiver, filter);
-            // We need WRITE_SMS permission to handle messages in
-            // actionMessageSentDisconnected()
-            registerReceiver(mMapReceiver, filterMessageSent,
-                             Manifest.permission.WRITE_SMS, null);
         } catch (Exception e) {
             Log.w(TAG,"Unable to register map receiver",e);
         }
