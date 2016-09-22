baf3b9b6c40e0e940a82e3651e4705fdf5a2b5d4
Pavlin Radoslavov <pavlin@google.com>
2016-09-21 17:46:07 -0700

    Enable debug logs for Bluetooth AdapterService
    Enabled log messages that are useful to debug the AdapterService state.
    Remaining log messages are controlled by AdapterService.VERBOSE flag
    and are disabled by default.
    
    Bug: 31442739
    Test: manual, and "frameworks/base/core/tests/bluetoothtests" unit tests
    Change-Id: I7d81fd605f64e64f29aee37314821384234b286d
    (cherry picked from commit 32a92489e9779d10c613a57d86135a7de6d3d7c4)

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -87,7 +87,8 @@ import com.android.internal.app.IBatteryStats;
 
 public class AdapterService extends Service {
     private static final String TAG = "BluetoothAdapterService";
-    private static final boolean DBG = false;
+    private static final boolean DBG = true;
+    private static final boolean VERBOSE = false;
     private static final boolean TRACE_REF = false;
     private static final int MIN_ADVT_INSTANCES_FOR_MA = 5;
     private static final int MIN_OFFLOADED_FILTERS = 10;
@@ -2382,11 +2383,12 @@ public class AdapterService extends Service {
             }
         }
 
-        debugLog("energyInfoCallback() status = " + status +
-                "tx_time = " + tx_time + "rx_time = " + rx_time +
-                "idle_time = " + idle_time + "energy_used = " + energy_used +
-                "ctrl_state = " + ctrl_state +
-                "traffic = " + Arrays.toString(data));
+        verboseLog("energyInfoCallback() status = " + status +
+                   "tx_time = " + tx_time + "rx_time = " + rx_time +
+                   "idle_time = " + idle_time +
+                   "energy_used = " + energy_used +
+                   "ctrl_state = " + ctrl_state +
+                   "traffic = " + Arrays.toString(data));
     }
 
     private int getIdleCurrentMa() {
@@ -2432,8 +2434,8 @@ public class AdapterService extends Service {
         enforceCallingOrSelfPermission(android.Manifest.permission.DUMP, TAG);
 
         if (args.length > 0) {
-            debugLog("dumpsys arguments, check for protobuf output: " +
-                    TextUtils.join(" ", args));
+            verboseLog("dumpsys arguments, check for protobuf output: "
+                       + TextUtils.join(" ", args));
             if (args[0].startsWith("--proto")) {
                 if (args[0].equals("--proto-java-bin")) {
                     dumpJava(fd);
@@ -2500,6 +2502,10 @@ public class AdapterService extends Service {
 
     private void debugLog(String msg) {
         if (DBG) Log.d(TAG, msg);
+    }
+
+    private void verboseLog(String msg) {
+        if (VERBOSE) Log.v(TAG, msg);
     }
 
     private void errorLog(String msg) {
